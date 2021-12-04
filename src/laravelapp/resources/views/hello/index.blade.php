834922557158f1de7e3281e7a47129115d16294f
Soh1121 <satou.shg@gmail.com>
2021-12-04 15:42:25 +0900

    [update]継承レイアウトの作成方法を学習
    レイアウト用テンプレートを継承するため

--- src/laravelapp/resources/views/hello/index.blade.php
+++ src/laravelapp/resources/views/hello/index.blade.php
@@ -1,36 +1,17 @@
-<html>
+@extends('layouts.helloapp')
 
-<head>
-	<title>Hello/Index</title>
-	<style>
-		body {
-			font-size: 16pt;
-			color: #999;
-		}
+@section('title', 'Index')
 
-		h1 {
-			font-size: 50pt;
-			text-align: right;
-			color: #f6f6f6;
-			margin: -20px 0px -30px 0px;
-			letter-spacing: -4pt;
-		}
-	</style>
-</head>
+@section('menubar')
+@parent
+インデックスページ
+@endsection
 
-<body>
-	<h1>Blade/Index</h1>
-	<p>&#064;whileディレクティブの例</p>
-	<ol>
-		@php
-		$counter = 0;
-		@endphp
-		@while ($counter < count($data)) <li>{{ $data[$counter] }}</li>
-			@php
-			$counter++;
-			@endphp
-			@endwhile
-	</ol>
-</body>
+@section('content')
+<p>ここが本文のコンテンツです。</p>
+<p>必要なだけ記述できます。</p>
+@endsection
 
-</html>
+@section('footer')
+copyright 2017 tuyano.
+@endsection
