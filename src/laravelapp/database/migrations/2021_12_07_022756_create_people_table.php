0562dc5b7018590b815b8b2c428005e003f2d904
Soh1121 <satou.shg@gmail.com>
2021-12-07 11:44:23 +0900

    [update]テーブル生成の処理を学習
    テーブルを生成するため

--- src/laravelapp/database/migrations/2021_12_07_022756_create_people_table.php
+++ src/laravelapp/database/migrations/2021_12_07_022756_create_people_table.php
@@ -6,14 +6,15 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreatePeopleTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
-        //
+        Schema::create('people', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('mail');
+            $table->integer('age');
+            $table->timestamps();
+        });
     }
 
     /**
