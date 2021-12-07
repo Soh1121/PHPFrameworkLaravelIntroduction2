08d7d66461c7a700acb8f1f0f1c4b1b447139671
Soh1121 <satou.shg@gmail.com>
2021-12-07 13:40:59 +0900

    [add]IDによる検索を学習
    モデルからレコードを検索するため

--- src/laravelapp/routes/web.php
+++ src/laravelapp/routes/web.php
@@ -13,6 +13,8 @@
 
 use App\Http\Middleware\HelloMiddleware;
 use App\Http\Controllers\HelloController;
+use App\Http\Controllers\PersonController;
+use App\Person;
 
 Route::get('/', function () {
     return view('welcome');
@@ -29,3 +31,5 @@ Route::post('hello/del', 'HelloController@remove');
 Route::get('hello/show', 'HelloController@show');
 
 Route::get('person', 'PersonController@index');
+Route::get('person/find', 'PersonController@find');
+Route::post('person/find', 'PersonController@search');
