5019024c69fd5449af64533987aa75d735dfc990
Soh1121 <satou.shg@gmail.com>
2021-12-09 14:43:42 +0900

    [update]paginateメソッドの利用方法を学習
    ペジネーションを用いるため

--- src/laravelapp/app/Http/Controllers/HelloController.php
+++ src/laravelapp/app/Http/Controllers/HelloController.php
@@ -11,10 +11,8 @@ class HelloController extends Controller
     public function index(Request $request)
     {
         $sort = $request->sort;
-        // $items = DB::table('people')->orderBy($sort, 'asc')
-        //     ->simplePaginate(5);
         $items = Person::orderBy($sort, 'asc')
-            ->simplePaginate(5);
+            ->paginate(5);
         $param = ['items' => $items, 'sort' => $sort];
         return view('hello.index', $param);
     }
